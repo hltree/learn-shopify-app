08c954d4dbf980d050469b612136ad2d3be9ed4f
hltree <35206336+hltree@users.noreply.github.com>
2022-04-20 09:45:18 +0900

    update: アクセストークンを取得するメソッドを作成

--- data/app/Models/Options.php
+++ data/app/Models/Options.php
@@ -10,4 +10,17 @@ class Options extends Model
     use HasFactory;
 
     protected $guarded = [];
+
+    public function getAccessToken() :string
+    {
+        $accessToken = '';
+
+        $where = $this::whereNotNull('access_token');
+        if ($where->exists()) {
+            $takeOne = $where->get()->take(1)[0];
+            $accessToken = $takeOne->getAttribute('access_token');
+        }
+
+        return $accessToken;
+    }
 }
